2c42b1243d7f832efae7a527bac9bd7f3b2d8d57
Endrit Ushe <usheendri@gmail.com>
2024-03-09 19:57:48 +0100

    final project starts here

---
+++ src/test/java/com/menuapp/menuapp1/MenuApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menuapp.menuapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
